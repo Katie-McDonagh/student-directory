6cc6c7b96a6dec71cc75d6e5e679e20045b3235b
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-08 14:32:04 +0100

    save/load code block added

--- more_exercises.rb
+++ more_exercises.rb
@@ -78,23 +78,23 @@ end
 
 def save_students
   # open the file for writing
-  file = File.open("students.csv", "w")
+  file = File.open("students.csv", "w") do |file|
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
-  file.close
+end
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
+  file = File.open(filename, "r") do |file|
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     student_array(name, cohort)
   end
-  file.close
+end
 end
 
 def try_load_students
